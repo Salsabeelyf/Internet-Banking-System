a8189339ebc1605177dec553e1a2cff075c60df5
Alaa Othman <a.g.abdelatief@gmail.com>
2019-05-17 04:31:25 +0200

    Initiate Transfer page
    Files related to this page are Transfer.html , Transfer_css.css and Transfer_js.js

---
+++ Development/FrontEnd/Transfer_js.js
@@ -0,0 +1,89 @@
+function Information()
+{
+ //from Database
+	  var UserName= "Alaa Gamal ";
+	    document.getElementById("username").innerHTML= UserName ;
+}
+
+
+function validateForm()
+{
+	 //from Database
+	var typeofaccount = "current";
+	var balance = 1044;
+	
+	var amount = document.getElementById("amountTransfer").value;
+    var AccountID  = document.getElementById("Account_id").value;
+
+
+	/*___________________________________Amount__________________________________*/ 
+	 if (amount == "" )    		
+		 {
+			 alert("1you don't have the required amount , please check your balance"); 
+			 return false;  
+		}
+		
+	 else if (isNaN(amount) )    		
+		 {
+			 alert("2you don't have the required amount , please check your balance") ; 
+			 return false;
+		}
+		
+	 else if (amount < 50 )          
+		 {
+			 alert("3Min amount is 50LE , please check your balance") ; 
+			 return false; 
+		}
+		
+	 else if ( ( typeofaccount == "saving") && (amount > 100000 ) )
+	 {
+		 
+		alert("You have exceeded the amount of money allowed to be transfer") ; 
+		return false; 
+	 }
+	 else if ( ( typeofaccount == "current") & (amount > 10000 )  )
+	 {
+		 
+		alert("You have exceeded the amount of money allowed to be transfer") ; 
+		return false; 
+	 }
+	 
+	else if (amount > balance)
+	 {
+		 alert("You don't have the required amount , please check your balance") ; 
+		return false; 
+	 }
+
+	 	/*___________________________________Account__________________________________*/ 
+		
+	 	 else if (AccountID == "" )    		
+		 {
+			 alert("You have entered an invalid account , please check your balance"); 
+			 return false;  
+		}
+			 else if (isNaN(AccountID) )    		
+		 {
+			 alert("You have entered an invalid account , please check your balance") ; 
+			 return false;
+		}
+		
+		else if(AccountID.length !=14)
+		{
+			alert("You have entered an invalid account , please check your balance") ; 
+			 return false;
+		}
+		
+		else
+		{
+			return true;
+		}
+		
+}
+
+
+
+		
+		
+		
+		
+		
