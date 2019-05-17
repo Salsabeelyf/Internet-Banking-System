e19ec0c1925e501d8e1a2ba7c667e20851d5a1e5
Alaa Othman <a.g.abdelatief@gmail.com>
2019-05-17 20:09:16 +0200

    Initiate Feature of Previous Transaction
    Files relatd to this feature are Display_PTransaction.html , Display_PTransaction_js.js , PreviousTransaction.html
    PreviousTransaction_css.css and PreviousTransaction_js.js

---
+++ Development/FrontEnd/Display_PTransaction_js.js
@@ -0,0 +1,78 @@
+ function myfunction( )
+	   {
+			
+			var getDisplayed= document.getElementById("optradio").value
+   
+			  var  Transaction =[  {amount:12345, From:1111 , to: 2222} , {amount:67890, From: 3333 , to: 4444} , {amount:78787, From: 5555 , to: 6666} 
+			   ,{amount:12345, From:1111 , to: 2222} , {amount:67890, From: 3333 , to: 4444} , {amount:78787, From: 5555 , to: 6666} ] ;
+			  var numbers= Transaction.length ;
+			   
+			   if(numbers < getDisplayed )
+			   {
+					change(numbers, Transaction);
+			   }
+			   
+			   else 
+			   {
+			   
+				    var num= numbers ;
+					while( num /  getDisplayed !=0)
+					{
+						change(getDisplayed, Transaction);
+						num = num -  getDisplayed;
+					}
+					if(num>0)
+					{
+					change(num, Transaction);
+					}
+					
+					
+			   }
+		 }
+			   
+		function change(n,Transaction)
+	   {
+	   
+	   var tablebody = document.getElementById("maintablebody");
+      removerows(tablebody);
+      addrows(tablebody, n,Transaction);
+	   }
+
+         function removerows (tablebody)
+		 {
+      var rows = tablebody.getElementsByTagName("tr");
+      while (rows.length)
+        rows[0].parentNode.removeChild(rows[0]);
+		}	
+
+  function addrows (tablebody, n , Transaction) 
+  {
+      for (var i=0; i<n; i++) 
+	  {	
+	         var row = document.createElement("tr");
+	        var titlecell= document.createElement("td");
+	        var money = document.createElement("h5");
+			money.innerHTML = Transaction[i].amount;
+			titlecell.appendChild(money);
+			row.appendChild(titlecell);
+			tablebody.appendChild(row); 
+
+      var titlecell2= document.createElement("td");
+	   var Fromm = document.createElement("h5");
+	   Fromm.innerHTML = Transaction[i].From;
+	   titlecell2.appendChild(Fromm);
+	    row.appendChild(titlecell2);
+		 tablebody.appendChild(row); 
+		 
+		 
+		 	   var titlecell3= document.createElement("td");
+	   var Too = document.createElement("h5");
+	   Too.innerHTML = Transaction[i].to; 
+	   titlecell3.appendChild(Too);
+	    row.appendChild(titlecell3);
+		 tablebody.appendChild(row); 			
+	}	  
+			   
+			   
+}	   
+	
